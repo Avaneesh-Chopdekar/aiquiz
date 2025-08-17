b977a860568dee80d5c40959cf6419d6b1033579
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-08-17 22:03:14 +0530

    created user entity

--- apps/api/src/app/users/entities/user.entity.ts
+++ apps/api/src/app/users/entities/user.entity.ts
@@ -1,1 +1,10 @@
-export class User {}
+export interface User {
+  id: string;
+  username: string;
+  email: string;
+  emailVerified: boolean;
+  passwordHash: string;
+  role: 'STUDENT' | 'TEACHER' | 'ADMIN';
+  createdAt: Date;
+  updatedAt: Date;
+}
