30a05382e968c050b7b835dd0db83b3d4798b78c
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-08-17 15:35:01 +0530

    fixed role types and updatedAt's $onUpdateFn() method for users table

--- apps/api/src/app/drizzle/schema/user.schema.ts
+++ apps/api/src/app/drizzle/schema/user.schema.ts
@@ -1,3 +1,4 @@
+import { sql } from 'drizzle-orm';
 import {
   pgTable,
   varchar,
@@ -14,7 +15,13 @@ export const users = pgTable('users', {
   email: varchar('email', { length: 50 }).notNull().unique(),
   emailVerified: boolean('email_verified').notNull(),
   passwordHash: varchar('password_hash', { length: 255 }).notNull(),
-  role: varchar('role', { enum: ['USER', 'ADMIN'], length: 50 }).notNull(),
+  role: varchar('role', {
+    enum: ['STUDENT', 'TEACHER', 'ADMIN'],
+    length: 50,
+  }).notNull(),
   createdAt: timestamp('created_at', { mode: 'string' }).notNull().defaultNow(),
-  updatedAt: timestamp('updated_at', { mode: 'string' }).notNull().defaultNow(),
+  updatedAt: timestamp('updated_at', { mode: 'string' })
+    .notNull()
+    .defaultNow()
+    .$onUpdateFn(() => sql`CURRENT_TIMESTAMP`),
 });
