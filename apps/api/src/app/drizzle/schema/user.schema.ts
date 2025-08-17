3947a8ea5174f35448fea9df8ea2b92656ce403d
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-08-17 12:57:26 +0530

    setup drizzle orm in api

---
+++ apps/api/src/app/drizzle/schema/user.schema.ts
@@ -0,0 +1,20 @@
+import {
+  pgTable,
+  varchar,
+  uuid,
+  timestamp,
+  boolean,
+} from 'drizzle-orm/pg-core';
+
+export const users = pgTable('users', {
+  id: uuid('id')
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  username: varchar('username', { length: 50 }).notNull().unique(),
+  email: varchar('email', { length: 50 }).notNull().unique(),
+  emailVerified: boolean('email_verified').notNull(),
+  passwordHash: varchar('password_hash', { length: 255 }).notNull(),
+  role: varchar('role', { enum: ['USER', 'ADMIN'], length: 50 }).notNull(),
+  createdAt: timestamp('created_at', { mode: 'string' }).notNull().defaultNow(),
+  updatedAt: timestamp('updated_at', { mode: 'string' }).notNull().defaultNow(),
+});
