5c413c9b2d0f7a3baa632ce6ff915bae86c3a76c
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-08-19 01:09:43 +0530

    added BootstrapAuth() component to check user authentication state

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -4,10 +4,23 @@ import Login from './pages/login';
 import Signup from './pages/signup';
 import NavBar from './components/navbar';
 import { ProtectedRoute } from './components/protected-route';
+import { useAuthStore } from './store';
+import { useEffect } from 'react';
+
+function BootstrapAuth() {
+  const refreshUser = useAuthStore((s) => s.refreshUser);
+
+  useEffect(() => {
+    refreshUser();
+  }, [refreshUser]);
+
+  return null;
+}
 
 export function App() {
   return (
     <>
+      <BootstrapAuth />
       <NavBar />
       <Routes>
         <Route
