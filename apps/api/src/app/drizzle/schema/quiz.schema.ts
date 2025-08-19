5b95649aaad5e017f9ae3e10e608099a687a0f11
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-08-19 21:32:07 +0530

    added slug field to quiz table and added index to title and slug

--- apps/api/src/app/drizzle/schema/quiz.schema.ts
+++ apps/api/src/app/drizzle/schema/quiz.schema.ts
@@ -1,18 +1,35 @@
 import { sql } from 'drizzle-orm';
-import { pgTable, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
+import {
+  index,
+  pgTable,
+  timestamp,
+  uniqueIndex,
+  uuid,
+  varchar,
+} from 'drizzle-orm/pg-core';
 import { users } from './user.schema';
 
-export const quizzes = pgTable('quizzes', {
-  id: uuid('id')
-    .primaryKey()
-    .$defaultFn(() => crypto.randomUUID()),
-  userId: uuid('user_id')
-    .notNull()
-    .references(() => users.id),
-  title: varchar('title', { length: 50 }).notNull(),
-  createdAt: timestamp('created_at', { mode: 'string' }).notNull().defaultNow(),
-  updatedAt: timestamp('updated_at', { mode: 'string' })
-    .notNull()
-    .defaultNow()
-    .$onUpdateFn(() => sql`CURRENT_TIMESTAMP`),
-});
+export const quizzes = pgTable(
+  'quizzes',
+  {
+    id: uuid('id')
+      .primaryKey()
+      .$defaultFn(() => crypto.randomUUID()),
+    userId: uuid('user_id')
+      .notNull()
+      .references(() => users.id),
+    title: varchar('title', { length: 50 }).notNull(),
+    slug: varchar('slug').unique().notNull(),
+    createdAt: timestamp('created_at', { mode: 'string' })
+      .notNull()
+      .defaultNow(),
+    updatedAt: timestamp('updated_at', { mode: 'string' })
+      .notNull()
+      .defaultNow()
+      .$onUpdateFn(() => sql`CURRENT_TIMESTAMP`),
+  },
+  (table) => [
+    index('title_idx').on(table.title),
+    uniqueIndex('slug_idx').on(table.slug),
+  ]
+);
